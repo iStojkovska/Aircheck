2bfc71d16f835f66a513818cd9fd483682fb9569
frosinastojanovska <stojanovska.frose@gmail.com>
2016-04-23 00:26:47 +0200

    Delete dbo.Table.sql

--- dbo.Table.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE dbo.Doctors
-(
-	ID INT NOT NULL,  
-	NAME varchar(20) NOT NULL,
-	SURNAME varchar(20) NOT NULL,
-	EMAIL varchar(30) NOT NULL,
-	PHONE varchar(10) NOT NULL,
-	LOCATION varchar(20) NOT NULL
-)
