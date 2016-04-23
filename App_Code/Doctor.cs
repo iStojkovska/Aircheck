33ffe89c29eb48e4481124ca50f276720ea18255
frosinastojanovska <stojanovska.frose@gmail.com>
2016-04-23 11:36:30 +0200

    Add code to Doctor.cs

--- App_Code/Doctor.cs
+++ App_Code/Doctor.cs
@@ -6,12 +6,25 @@ using System.Web;
 /// <summary>
 /// Summary description for Doctor
 /// </summary>
+[Serializable]
 public class Doctor
 {
-    public Doctor()
+    public string name;
+    public string email;
+    public string phone;
+    public string city;
+    public string address;
+    public string hospital;
+    public string addmitingTimes;
+
+    public Doctor(string Name, string Email, string Phone, string City, string Address, string Hospital, string AddmitingTimes)
     {
-        //
-        // TODO: Add constructor logic here
-        //
+        name = Name;
+        email = Email;
+        phone = Phone;
+        city = City;
+        address = Address;
+        hospital = Hospital;
+        addmitingTimes = AddmitingTimes;
     }
 }
